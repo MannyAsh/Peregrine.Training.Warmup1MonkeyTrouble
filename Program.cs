0608c4f79dc770939e865a2140388666cab02ede
MannyAsh <amaan.ashiq.aa@gmail.com>
2023-09-15 15:45:55 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Warmup1MonkeyTrouble
+{
+    internal class Program
+    {
+        static void Main(string[] args) {
+
+            Console.WriteLine(monkeyTrouble(true,true));
+            Console.WriteLine(monkeyTrouble(false, false));
+            Console.WriteLine(monkeyTrouble(true, false));
+            Console.WriteLine(monkeyTrouble(false, true));
+            Console.ReadLine();
+
+
+        }
+
+        static Boolean monkeyTrouble(Boolean aSmile, Boolean bSmile) {
+
+            if (aSmile == true && bSmile == true || aSmile == false && bSmile == false)
+            {
+
+                return true;
+            }
+
+            else {
+
+                return false;
+            
+            }
+
+        
+        }
+    }
+}
